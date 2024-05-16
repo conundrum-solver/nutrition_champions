2b72a55a8ee3613d8923f6ac51d30f183d8424c2
conundrum-solver <alsagr_88@hotmail.com>
2024-05-16 21:54:29 +0100

    Add dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,10 @@
 from django_plotly_dash import DjangoDash
 from dash import *
-from datetime import date
+from datetime import date, datetime
 from django.shortcuts import render
+from django.db.models import Count
+from attendance.models import ScanRecord
+from django.utils import timezone
 
 
 def dashboard_view(request):
@@ -9,10 +12,10 @@ def dashboard_view(request):
     return render(request, 'attendance_dashboard.html')
 
 
-dashboard_app = DjangoDash('StudentScanApp')  # Create a new Dash app
+app = DjangoDash('dashboard')  # Create a new Dash app
 
 # Define layout of the Dash app
-dashboard_app.layout = html.Div([
+app.layout = html.Div([
     html.H1('Student Scan Data'),
     dcc.Graph(id='scan-graph'),
     dcc.DatePickerRange(
@@ -24,13 +27,36 @@ dashboard_app.layout = html.Div([
 
 
 # Define callback to update graph based on selected date range
-@dashboard_app.callback(
+@app.callback(
     Output('scan-graph', 'figure'),
     [Input('date-picker', 'start_date'),
      Input('date-picker', 'end_date')]
 )
-def update_graph(start_date, end_date):
+def update_graph (start_date, end_date):
     # Query student scan data from Django database based on selected date range
+    # Here you will write the logic to query the data from your Django model
+    # For demonstration purposes, let's assume you have a StudentScan model
+    # and you want to query data between the selected date range
+    start_date = timezone.make_aware(datetime.strptime(start_date, "%Y-%m-%d"))
+    end_date = timezone.make_aware(datetime.strptime(end_date, "%Y-%m-%d"))
+    student_scan_data = ScanRecord.objects.filter(timestamp__range=(start_date, end_date))
+    scan_counts = student_scan_data.values('timestamp__date').annotate(count=Count('id')).order_by('timestamp__date')
+
     # Process data and create Plotly figure
+    # For demonstration purposes, let's assume you want to create a simple bar chart
+    x_values = [entry['timestamp__date'] for entry in scan_counts]
+    y_values = [entry['count'] for entry in scan_counts]
+    figure = {
+        'data': [
+            {'x': x_values, 'y': y_values, 'type': 'bar', 'name': 'Student Scans'}
+        ],
+        'layout': {
+            'title': 'Student Scan Data',
+            'xaxis': {'title': 'timestamp'},
+            'yaxis': {'title': 'Scan Count'},
+            "height": 1700,  # px
+        }
+    }
+
     # Return updated figure to be displayed in the graph
-    pass
+    return figure
